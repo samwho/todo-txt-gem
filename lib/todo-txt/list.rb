768760318967dd9cfe1f928047dbca8c648a1f3a
Sam Rose <samwho@lbak.co.uk>
2011-08-19 17:19:12 +0100

    Added more functionality to Todo::List and added more specs.

--- lib/todo-txt/list.rb
+++ lib/todo-txt/list.rb
@@ -8,13 +8,41 @@ module Todo
     # You would initialize this object like do:
     #
     #   list = Todo::List.new "/home/sam/Dropbox/todo/todo-txt"
-    def initialize path
-      @path = path
+    #
+    # Alternately, you can initialize this object with an array of strings or
+    # tasks. If the array is of strings, the strings will be converted into
+    # tasks. You can supply a mixed list of string and tasks if you wish.
+    #
+    # Example:
+    #
+    #   array = Array.new
+    #   array.push "(A) A string task!"
+    #   array.push Todo::Task.new("(A) An actual task!")
+    #
+    #   list = Todo::List.new array
+    def initialize list
+      if list.is_a? Array
+        # No file path was given.
+        @path = nil
 
-      # Read in lines from file, create Todo::Tasks out of them and push them
-      # onto self.
-      File.open(path) do |file|
-        file.each_line { |line| self.push Todo::Task.new line }
+        # If path is an array, loop over it, adding to self.
+        list.each do |task|
+          # If it's a string, make a new task out of it.
+          if task.is_a? String
+            self.push Todo::Task.new task
+          # If it's a task, just add it.
+          elsif task.is_a? Todo::Task
+            self.push task
+          end
+        end
+      elsif list.is_a? String
+        @path = list
+
+        # Read in lines from file, create Todo::Tasks out of them and push them
+        # onto self.
+        File.open(list) do |file|
+          file.each_line { |line| self.push Todo::Task.new line }
+        end
       end
     end
 
@@ -22,6 +50,38 @@ module Todo
     # Todo::List object.
     def path
       @path
+    end
+
+    # Filters the list by priority and returns a new list.
+    #
+    # Example:
+    #
+    #   list = Todo::List.new "/path/to/list"
+    #   list.by_priority "A" #=> Will be a new list with only priority A tasks
+    def by_priority priority
+      Todo::List.new self.select { |task| task.priority == priority }
+    end
+
+    # Filters the list by context and returns a new list.
+    #
+    # Example:
+    #
+    #   list = Todo::List.new "/path/to/list"
+    #   list.by_context "@context" #=> Will be a new list with only tasks
+    #                                  containing "@context"
+    def by_context context
+      Todo::List.new self.select { |task| task.contexts.include? context }
+    end
+
+    # Filters the list by project and returns a new list.
+    #
+    # Example:
+    #
+    #   list = Todo::List.new "/path/to/list"
+    #   list.by_project "+project" #=> Will be a new list with only tasks
+    #                                  containing "+project"
+    def by_project project
+      Todo::List.new self.select { |task| task.projects.include? project }
     end
   end
 end
--- spec/todo-txt/list_spec.rb
+++ spec/todo-txt/list_spec.rb
@@ -14,4 +14,58 @@ describe Todo::List do
     # This is a little bit fragile but it helps me sleep at night.
     list[0].priority.should == "A"
   end
+
+  it 'should be able to filter by priority' do
+    list.by_priority("A").each do |task|
+      task.priority.should == "A"
+    end
+
+    # Make sure some data was actually checked
+    list.by_priority("A").length.should be > 0
+  end
+
+  it 'should be able to filter by context' do
+    list.by_context("@context").each do |task|
+      task.contexts.should include "@context"
+    end
+
+    # Make sure some data was actually checked
+    list.by_context("@context").length.should be > 0
+  end
+
+  it 'should be able to filter by project' do
+    list.by_project("+project").each do |task|
+      task.projects.should include "+project"
+    end
+
+    # Make sure some data was actually checked
+    list.by_project("+project").length.should be > 0
+  end
+
+  it 'should be able to filter by project, context and priority' do
+    filtered = list.by_project("+project").
+                    by_context("@context").
+                    by_priority("C")
+
+    filtered.each do |task|
+      task.projects.should include "+project"
+      task.contexts.should include "@context"
+      task.priority.should == "C"
+    end
+
+    # Make sure some data was actually checked
+    filtered.length.should be > 0
+  end
+
+  it 'should be sortable' do
+    list.sort.each_cons(2) do |task_a, task_b|
+      task_a.should be <= task_b
+    end
+
+    # Make sure some data was actually checked
+    list.sort.length.should be > 0
+
+    # Class should still be Todo::List
+    list.sort.class.should == Todo::List
+  end
 end
