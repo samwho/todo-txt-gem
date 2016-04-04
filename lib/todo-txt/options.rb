4d0f3f751c535695db0fef8e79a8522faa8a5f86
Mark Rickerby <me@maetl.net>
2016-04-05 02:52:31 +1000

    Add customizable options
    This provides the capability to set custom options to control
    various aspects of list and task behaviour.
    
    It’s filled in with the following suggested options by default:
    
    - require_completed_on
    - maintain_field_order

---
+++ lib/todo-txt/options.rb
@@ -0,0 +1,35 @@
+module Todo
+  class << self
+    attr_accessor :options_instance
+  end
+
+  def self.options
+    self.options_instance ||= Options.new
+  end
+
+  def self.customize
+    self.options_instance ||= Options.new
+    yield(options_instance)
+  end
+
+  class Options
+    # Require all done tasks to have a `completed_on` date. True by default.
+    #
+    # - When `true`, tasks with invalid dates are considered not done.
+    # - When `false`, tasks starting with `x ` are considered done.
+    attr_accessor :require_completed_on
+
+    # Whether or not to preserve original field order for roundtripping.
+    attr_accessor :maintain_field_order
+
+    def initialize
+      reset
+    end
+
+    # Reset to defaults.
+    def reset
+      @require_completed_on = true
+      @maintain_field_order = false
+    end
+  end
+end
