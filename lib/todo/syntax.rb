6b0263a534c731403161817bf7fe5e7e0d5efe2b
André Lamelas <andre@lamelas.org>
2017-05-31 16:43:34 +0100

    Allow tags to contain uppercase letters.

--- lib/todo/syntax.rb
+++ lib/todo/syntax.rb
@@ -22,7 +22,7 @@ module Todo
     DUE_ON_PATTERN = /(?:due:)(\d{4}-\d{2}-\d{2})(?:\s+|$)/i
 
     # The regex used to match generic tags.
-    TAGS_PATTERN = /([a-z]+):([a-z0-9_-]+)/i
+    TAGS_PATTERN = /([a-z]+):([A-Za-z0-9_-]+)/i
 
     # Extracts the readable text content of a task line, stripping out all the
     # discrete pieces of metadata (priority, dates, completion flag, projects,
